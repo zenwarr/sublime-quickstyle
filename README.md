7b9c3f0edff28230100a32e8ca8ec6d198561c48
zenwarr <zenw@yandex.ru>
2015-06-19 17:34:03 +0600

    improve it

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Sublime QuickStyle
+# Sublime QuickStyle
 
 Open HTML and CSS files, position cursor at class name and select "Add rule from class name " in command palette. Now generated rule will be inserted in the opened CSS file view at the point where cursor is located.
+
+Supports plain CSS as well as SCSS (regular and indented syntax), LESS and Stylus. By default, generates rules for Stylus in indented syntax, but you can override this by setting "stylus_use_indented" to false in quickstyle.sublime-settings file.
--- quickstyle.py
+++ quickstyle.py
@@ -2,36 +2,33 @@ import sublime, sublime_plugin
 import string
 
 whitelist = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-'
+style_extensions = ['css', 'sass', 'scss', 'styl', 'less']
+stylus_use_indented = True
 
 class QsAddRuleCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     self.cur_pos = self.view.sel()[0].a
-    class_or_id_name = self.get_class_or_id()
-    if len(class_or_id_name) == 0:
+    class_name = self.get_class_nane()
+    if len(class_name) == 0:
       sublime.error_message('No valid css class at this point')
       return
 
     target_view = self.find_target_view()
     if target_view == None:
-      sublime.error_message('No open css file found')
+      sublime.error_message('No open style file found')
       return
 
     sublime.active_window().focus_view(target_view)
 
-    target_sel = target_view.sel()[0]
-    target_sel_start = target_sel.a
+    generator = QsRuleGenerator(target_view)
+    generated_rule = generator.generate(target_view.sel()[0].a, class_name)
+    if generated_rule == None or len(generated_rule) == 0:
+      sublime.error_message('Error while generating rule')
+      return
 
-    gen_rule = '.{0} {{\n  \n}}'.format(class_or_id_name)
-    target_view.insert(edit, target_sel_start, gen_rule)
+    generator.insert(edit, target_view.sel()[0].a, generated_rule)
 
-    cursor_region = sublime.Region(target_sel_start + len(gen_rule) - 2)
-
-    target_view.sel().clear()
-    target_view.sel().add(cursor_region)
-
-    target_view.show(cursor_region)
-
-  def get_class_or_id(self):
+  def get_class_nane(self):
     line = self.view.line(self.cur_pos)
     line_text = self.view.substr(line)
     if line_text == None:
@@ -68,5 +65,98 @@ class QsAddRuleCommand(sublime_plugin.TextCommand):
     all_views = win.views()
 
     for view in all_views:
-      if view.file_name().endswith('.css'):
-        return view
+      file_name = view.file_name()
+      for ext in style_extensions:
+        if file_name.endswith('.' + ext):
+          return view
+
+class QsRuleGenerator:
+  def __init__(self, view):
+    self.view = view
+
+    view_settings = self.view.settings()
+    self.tabWidth = view_settings.get('tab_size')
+    self.useTabs = not view_settings.get('translate_tabs_to_spaces')
+
+  def detect_syntax(self, point):
+    scopes = self.view.scope_name(point).split(' ')
+    for scope in scopes:
+      if scope == 'source.stylus':
+        return 'stylus-indented' if stylus_use_indented else 'stylus'
+      elif scope == 'source.scss':
+        return 'scss'
+      elif scope == 'source.sass':
+        return 'sass'
+      elif scope == 'source.less':
+        return 'less'
+      elif scope == 'source.css':
+        return 'css'
+    return None
+
+  def detect_indentation_prefix(self, point):
+    line = self.view.line(point)
+    line_text = self.view.substr(line)
+
+    indent = ''
+    for char in line_text:
+      if char in ' \t':
+        indent += char
+
+    return indent
+
+  def get_indentation(self):
+    if self.useTabs:
+      return '\t'
+    else:
+      return ' ' * self.tabWidth
+
+  def concat(self, indentation, lines):
+    return '\n'.join([lines[0]] + [indentation + line for line in lines[1:]])
+
+  def generate(self, point, class_name):
+    syntax = self.detect_syntax(point)
+    if syntax == None:
+      sublime.error_message('Unsupported syntax in target file')
+      return
+
+    indentation = self.detect_indentation_prefix(point)
+
+    if syntax in ['css', 'scss', 'stylus', 'less']:
+      return self.concat(indentation, [
+               '.' + class_name + ' {',
+               self.get_indentation() + '$',
+               '}'
+                ]) + ('' if len(indentation) else '\n')
+    elif syntax in ['sass', 'stylus-indented']:
+      return self.concat(indentation, [
+                '.' + class_name,
+                self.get_indentation() + '$'
+                ]) + ('' if len(indentation) else '\n')
+    else:
+      raise Error()
+
+  def insert(self, edit, point, generated):
+    if generated == None or len(generated) == 0:
+      return
+
+    # find where we should place cursor after inserting, this position is indicated by $ sign
+    cursor_offset = generated.find('$')
+    if cursor_offset < 0:
+      cursor_offset = len(generated)
+
+    # now remove $ sign from generated string and insert it into view
+    generated = generated[:cursor_offset] + generated[cursor_offset+1:]
+    self.view.insert(edit, point, generated)
+
+    # locate cursor at the insertion point
+    cursor_region = sublime.Region(point + cursor_offset)
+    self.view.sel().clear()
+    self.view.sel().add(cursor_region)
+    self.view.show(cursor_region)
+
+
+def plugin_loaded():
+  global stylus_use_indented
+
+  qs_settings = sublime.load_settings('quickstyle.sublime-settings')
+  stylus_use_indented = qs_settings.get('stylus_use_indented', True)
